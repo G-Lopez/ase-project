f4667632a656743dea281498441d21b7262713b0
G-Lopez <gal2129@columbia.edu>
2017-12-08 14:26:37 -0500

    pls

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/', protected, (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/*.(js|css|jpg|png)', protected, (req, res) => {
+app.get('/*.(js|css|jpg|png|ico)', protected, (req, res) => {
   res.sendFile(__dirname + '/views' + req.url)
 })
 
