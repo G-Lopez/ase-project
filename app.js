66cf1ca90583eee00f21c301695c05def19f126d
Gabriel_Lopez <gal2129@columbia.edu>
2017-12-09 17:44:22 -0500

    DEPLOYMENT WORKS WOO

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const auth = require('./auth')
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 const GOOGLE_API_KEY = 'key=AIzaSyAqcJLiH6zsbV4Cc3wxs454_DBqSg5lEy4'
 const DB_URI = 'mongodb://gabriel:gabrielase@ds125716.mlab.com:25716/ase-project'
 const https = require('https');
 
 //Models
-const User = require('./models/user').User
-const Book = require('./models/book').Book
-const Request = require('./models/request').Request
+const User = require('./Models/user').User
+const Book = require('./Models/book').Book
+const Request = require('./Models/request').Request
 
 //Mongoose Connection
 const db = mongoose.connect(DB_URI, {useMongoClient: true})
